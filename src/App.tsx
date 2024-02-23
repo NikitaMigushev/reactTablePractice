94a74e21b0a02c6e66404f24bc6da2d82f498c01
Nikita Migushev <migushev@gmail.com>
2024-02-23 16:13:17 +0300

    react table

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Table from "@/components/Table";
+import {ColumnDef} from "@tanstack/react-table";
+
+function App() {
+
+    return (
+        <>
+            <div>
+                <Table />
+            </div>
+        </>
+    )
+}
+
+export default App
